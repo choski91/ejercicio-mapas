cf45c7ef7a8d8884ef73153477470058d9a0ff60
choski91 <mlaura.smichowski@gmail.com>
2025-07-17 20:06:13 +0200

    validación/css/renderizacion de funcion

--- script.js
+++ script.js
@@ -1,152 +1,84 @@
 var map = L.map('mapUno').setView([-34.60, -58.43], 5);
 
 var OpenStreetMap_CAT = L.tileLayer('https://tile.openstreetmap.bzh/ca/{z}/{x}/{y}.png', {
-maxZoom: 5,
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>'
+  maxZoom: 5,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>'
 }).addTo(map);
 
 var mapDos = L.map('mapaDos').setView([-34.60, -58.43], 5);
 
 var OpenStreetMap_CAT2 = L.tileLayer('https://tile.openstreetmap.bzh/ca/{z}/{x}/{y}.png', {
-maxZoom: 5,
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>'
+  maxZoom: 5,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>'
 }).addTo(mapDos);
 
-async function getData() {
+let earthquakeMarkersLayer = L.featureGroup().addTo(mapDos);
+
+const earthquakeIcons = {
+  yellow: L.icon({ iconUrl: './assets/icono amarillo.png', iconSize: [15, 20] }),
+  white: L.icon({ iconUrl: './assets/icono blanco.jpg', iconSize: [15, 20] }),
+  purple: L.icon({ iconUrl: './assets/icono morado.jpg', iconSize: [15, 20] }),
+  softOrange: L.icon({ iconUrl: './assets/icono naranja suave.jpg', iconSize: [15, 20] }),
+  orange: L.icon({ iconUrl: './assets/icono naranja.png', iconSize: [15, 20] }),
+  red: L.icon({ iconUrl: './assets/icono rojo.png', iconSize: [15, 20] }),
+  darkGreen: L.icon({ iconUrl: './assets/icono verde oscuro.png', iconSize: [15, 20] }),
+  green: L.icon({ iconUrl: './assets/icono verde.jpg', iconSize: [15, 20] })
+};
+function getEarthquakeIcon(magnitude) {
+  if (magnitude >= 7) {
+    return earthquakeIcons.purple;
+  } else if (magnitude >= 6 && magnitude < 7) {
+    return earthquakeIcons.red;
+  } else if (magnitude >= 5 && magnitude < 6) {
+    return earthquakeIcons.orange;
+  } else if (magnitude >= 4 && magnitude < 5) {
+    return earthquakeIcons.softOrange;
+  } else if (magnitude >= 3 && magnitude < 4) {
+    return earthquakeIcons.yellow;
+  } else if (magnitude >= 2 && magnitude < 3) {
+    return earthquakeIcons.green;
+  } else if (magnitude >= 1 && magnitude < 2) {
+    return earthquakeIcons.darkGreen;
+  } else if (magnitude >= 0 && magnitude < 1) {
+    return earthquakeIcons.white;
+  }
+}
+
+async function todosTerremotos() {
   try {
     const res = await fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson");
     const data = await res.json();
     return data.features;
-  } catch (error) { 
+  } catch (error) {
     console.log(error);
   }
 }
-getData().then(data => {
-    var yellowIcon = L.icon({
-    iconUrl: './assets/icono amarillo.png',
-    iconSize:[15, 20],
-})
-   var whiteIcon = L.icon({
-    iconUrl: './assets/icono blanco.jpg',
-    iconSize: [15, 20],
-   })
-    var purpleIcon = L.icon({
-    iconUrl: './assets/icono morado.jpg',
-    iconSize: [15, 20],
-   })
-    var softOrangeIcon = L.icon({
-    iconUrl: './assets/icono naranja suave.jpg',
-    iconSize: [15, 20],
-   })
-    var orangeIcon = L.icon({
-    iconUrl: './assets/icono naranja.png',
-    iconSize: [15, 20],
-   })
-    var redIcon = L.icon({
-    iconUrl: './assets/icono rojo.png',
-    iconSize: [15, 20],
-   })
-    var darkGreenIcon = L.icon({
-    iconUrl: './assets/icono verde oscuro.png',
-    iconSize: [15, 20],
-   })
-    var greenIcon = L.icon({
-    iconUrl: './assets/icono verde.jpg',
-    iconSize: [15, 20],
-   })
+async function getData(dateStartFiltrada, dateEndFiltrada, magnitudFiltradaMin, magnitudFiltradaMax) {
+  try {
+    const res = await fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${dateStartFiltrada}&endtime=${dateEndFiltrada}&minmagnitude=${magnitudFiltradaMin}&maxmagnitude=${magnitudFiltradaMax}`);
+    const data = await res.json();
+    return data.features;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-for (let i = 0; i < data.length; i++){ 
- if(data[i].properties.mag >= 7){
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: purpleIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
+todosTerremotos().then(data => {
+  for (let i = 0; i < data.length; i++) {
+
+    const coordenadas1 = [data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]];
+    const markerIcon = getEarthquakeIcon(data[i].properties.mag);
+    const marcador = L.marker(coordenadas1, { icon: markerIcon });
+
+    marcador.addTo(map)
+      .bindPopup(`${data[i].properties.title};
   Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
+  Fecha: ${new Date(data[i].properties.time).toLocaleString()};
   Código: ${data[i].properties.code};
   Ubicación: ${data[i].properties.place}
   `)
-.addTo(map);
-}
- else if(data[i].properties.mag >= 6 && data[i].properties.mag < 7){
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: redIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
-  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
-  Código: ${data[i].properties.code};
-  Ubicación: ${data[i].properties.place}
-  `)
-.addTo(map);
-}
- else if(data[i].properties.mag >=5 && data[i].properties.mag < 6){
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: orangeIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
-  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
-  Código: ${data[i].properties.code};
-  Ubicación: ${data[i].properties.place}
-  `)
-.addTo(map);
-}
- else if(data[i].properties.mag >=4 && data[i].properties.mag < 5){
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: softOrangeIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
-  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
-  Código: ${data[i].properties.code};
-  Ubicación: ${data[i].properties.place}
-  `)
-.addTo(map);
-}
- else if(data[i].properties.mag >= 3 && data[i].properties.mag < 4) {
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: yellowIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
-  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
-  Código: ${data[i].properties.code};
-  Ubicación: ${data[i].properties.place}
-  `)
-.addTo(map);
-}
- else if(data[i].properties.mag >=2 && data[i].properties.mag < 3 ){
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: greenIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
-  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
-  Código: ${data[i].properties.code};
-  Ubicación: ${data[i].properties.place}
-  `)
-.addTo(map);
-}
- else if(data[i].properties.mag >= 1 && data[i].properties.mag < 2 ){
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: darkGreenIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
-  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
-  Código: ${data[i].properties.code};
-  Ubicación: ${data[i].properties.place}
-  `)
-.addTo(map);
-}
- else if(data[i].properties.mag >= 0 && data[i].properties.mag < 1){
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas, {icon: whiteIcon}).addTo(map)
-.bindPopup(`${data[i].properties.title};
-  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
-  Fecha: ${data[i].properties.time};
-  Código: ${data[i].properties.code};
-  Ubicación: ${data[i].properties.place}
-  `)
-.addTo(map);
- }
-}
+  }
 });
-
 
 document.getElementById("mag").addEventListener("submit", function (event) {
   event.preventDefault();
@@ -160,21 +92,35 @@ document.getElementById("mag").addEventListener("submit", function (event) {
   const dateEndFiltrada = event.target.endDate.value;
   console.log(dateEndFiltrada)
 
-async function getData() {
-  try {
-    const res = await fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${dateStartFiltrada}&endtime=${dateEndFiltrada}&minmagnitude=${magnitudFiltradaMin}&maxmagnitude=${magnitudFiltradaMax}`);
-    const data = await res.json();
-    return data.features;
-  } catch (error) { 
-    console.log(error);
-  }
-}
-getData().then(data => {
-  console.log("pintando terremotos")
-  for (let i = 0; i < data.length; i++){ 
-  const coordenadas = [data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]];
-  const marker = L.marker(coordenadas).addTo(mapDos)
-  }
-});
+    if (!dateStartFiltrada) {
+        alert("Por favor, selecciona una fecha de inicio.");
+        return;
+    }
+
+    if (!dateEndFiltrada) {
+        alert("Por favor, selecciona una fecha de fin.");
+        return;
+    }
+
+    if (dateEndFiltrada < dateStartFiltrada) {
+        alert("La fecha de fin no puede ser anterior a la fecha de inicio.");
+        return;
+    }
+
+  earthquakeMarkersLayer.clearLayers();
+
+  //pasar los parametros de magnitud y fecha
+  getData(dateStartFiltrada, dateEndFiltrada, magnitudFiltradaMin, magnitudFiltradaMax).then(data => {
+    for (let i = 0; i < data.length; i++) {
+      const coordenadas = [data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]];
+      const marker = L.marker(coordenadas).addTo(earthquakeMarkersLayer)
+        .bindPopup(`${data[i].properties.title};
+  Magnitud: ${data[i].properties.mag}${data[i].properties.magType};
+  Fecha: ${new Date(data[i].properties.time).toLocaleString()};
+  Código: ${data[i].properties.code};
+  Ubicación: ${data[i].properties.place}
+  `)
+    }
+  })
 });
 
